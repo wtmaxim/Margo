9f7bb472565aae39f0d507ad7b75c138ee4a7314
wtmaxim <maxime.derame@hotmail.fr>
2016-04-05 13:48:44 +0200

    Config

---
+++ app/config/prod.php
@@ -0,0 +1,19 @@
+<?php
+
+// Timezone.
+date_default_timezone_set('Europe/Paris');
+
+// Cache
+$app['cache.path'] = __DIR__ . '/../cache';
+
+// Twig cache
+$app['twig.options.cache'] = $app['cache.path'] . '/twig';
+
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'host'     => 'localhost',
+    'dbname'   => 'margo2',
+    'user'     => 'root',
+    'password' => 'mysql',
+);
--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maxime
- * Date: 05/04/2016
- * Time: 13:40
- */
+
